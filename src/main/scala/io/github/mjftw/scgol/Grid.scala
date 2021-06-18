050b88d043a51914d157e73aedb322c303c0b344
Merlin Webster <mjftwebster@gmail.com>
2021-06-18 08:30:32 +0100

    Add code to check number of living neighbors at a location

--- src/main/scala/io/github/mjftw/scgol/Grid.scala
+++ src/main/scala/io/github/mjftw/scgol/Grid.scala
@@ -5,9 +5,10 @@ import cats.implicits._
 import io.github.mjftw.scgol.LifeState._
 
 case class Dimension(value: Int)
-case class Coordinate(x: Int, y: Int)
+case class Location(x: Int, y: Int)
+
 case class Grid(
-    values: Map[Coordinate, LifeState],
+    values: Map[Location, LifeState],
     height: Dimension,
     width: Dimension
 )
@@ -18,8 +19,8 @@ object Grid {
     val values = (for {
       x <- List.range(0, width)
       y <- List.range(0, height)
-    } yield (x, y)).foldLeft(Map.empty[Coordinate, LifeState])((m, xy) =>
-      m + (Coordinate.tupled(xy) -> isAlive)
+    } yield (x, y)).foldLeft(Map.empty[Location, LifeState])((m, xy) =>
+      m + (Location.tupled(xy) -> isAlive)
     )
 
     Grid(
@@ -27,6 +28,56 @@ object Grid {
       Dimension(width),
       Dimension(height)
     )
+  }
+  implicit class LocationOps(xy: Location) {
+    def to(endXY: Location): List[Location] =
+      for {
+        xs <- List.range(xy.x, endXY.x + 1)
+        ys <- List.range(xy.y, endXY.y + 1)
+      } yield Location(xs, ys)
+  }
+
+  implicit class GridOps(grid: Grid) {
+    def minX: Int = 0
+    def minY: Int = 0
+    def maxX: Int = grid.width.value - 1
+    def maxY: Int = grid.height.value - 1
+
+    def get(xy: Location): LifeState = grid.values(xy)
+
+    def set(xy: Location, lifeState: LifeState): Grid =
+      grid.copy(values = grid.values + (xy -> lifeState))
+
+    def livingInRange(fromXY: Location, toXY: Location): Int =
+      (fromXY to toXY)
+        .map(xy => grid.values(xy))
+        .filter(_ == Alive)
+        .length
+
+    def livingNeighborsAt(xy: Location): Int = {
+      val (fromXY, toXY) = xy match {
+        // Corners
+        case Location(x, y) if x <= minX && y <= minY => (Location(x, y), Location(x + 1, y + 1))
+        case Location(x, y) if x <= minX && y >= maxY => (Location(x, y - 1), Location(x + 1, y))
+        case Location(x, y) if x >= maxX && y <= minY => (Location(x - 1, y), Location(x, y + 1))
+        case Location(x, y) if x >= maxX && y >= maxY => (Location(x - 1, y - 1), Location(x, y))
+
+        // Edges
+        case Location(x, y) if x <= minX => (Location(x, y - 1), Location(x + 1, y + 1))
+        case Location(x, y) if y <= minY => (Location(x - 1, y), Location(x + 1, y + 1))
+        case Location(x, y) if x >= maxX => (Location(x - 1, y - 1), Location(x, y + 1))
+        case Location(x, y) if y >= maxY => (Location(x - 1, y - 1), Location(x + 1, y))
+
+        // Middle
+        case Location(x, y) => (Location(x - 1, y - 1), Location(x + 1, y + 1))
+      }
+
+      // Do not include the cell at xy in the count - it's not a neighbor
+      val thisCell = if (grid.get(xy) == Alive) 1 else 0
+
+      livingInRange(fromXY, toXY) - thisCell
+
+    }
   }
 
 }
--- src/test/scala/io/github/mjftw/scgol/GridSpec.scala
+++ src/test/scala/io/github/mjftw/scgol/GridSpec.scala
@@ -2,26 +2,116 @@ package io.mjftw.github.scgol
 
 import org.scalatest.flatspec.AnyFlatSpec
 import io.github.mjftw.scgol.Grid
+import io.github.mjftw.scgol.Grid._
 import org.scalatest.matchers.should.Matchers
 import io.github.mjftw.scgol.Dimension
-import io.github.mjftw.scgol.Dead
-import io.github.mjftw.scgol.Coordinate
+import io.github.mjftw.scgol.{Alive, Dead}
+import io.github.mjftw.scgol.Location
 
 class GridSpec extends AnyFlatSpec with Matchers {
   "make" should "create a new grid" in {
     Grid.make(3, 2, Dead) should be(
       Grid(
         Map(
-          Coordinate(0, 0) -> Dead,
-          Coordinate(0, 1) -> Dead,
-          Coordinate(1, 0) -> Dead,
-          Coordinate(1, 1) -> Dead,
-          Coordinate(2, 0) -> Dead,
-          Coordinate(2, 1) -> Dead
+          Location(0, 0) -> Dead,
+          Location(0, 1) -> Dead,
+          Location(1, 0) -> Dead,
+          Location(1, 1) -> Dead,
+          Location(2, 0) -> Dead,
+          Location(2, 1) -> Dead
         ),
         Dimension(3),
         Dimension(2)
       )
     )
+  }
+
+  "gridNeighborsAt" should "give an accurate count away from edges with living cell" in {
+    val grid =
+      (Location(2, 2) to Location(3, 4)).foldLeft {
+        Grid.make(9, 9, Dead)
+      } { (grid, xy) =>
+        grid.set(xy, Alive)
+      }
+
+    grid.livingNeighborsAt(Location(3, 3)) should be(5)
+  }
+
+  it should "give an accurate count away from edges with dead cell" in {
+    val grid =
+      (Location(2, 2) to Location(3, 4))
+        .foldLeft {
+          Grid.make(9, 9, Dead)
+        } { (grid, xy) =>
+          grid.set(xy, Alive)
+        }
+        .set(Location(3, 3), Dead)
+
+    grid.livingNeighborsAt(Location(3, 3)) should be(5)
+  }
+
+  it should "give an accurate count on left edge" in {
+    val grid =
+      (Location(0, 3) to Location(2, 7))
+        .foldLeft {
+          Grid.make(9, 9, Dead)
+        } { (grid, xy) =>
+          grid.set(xy, Alive)
+        }
+
+    grid.livingNeighborsAt(Location(0, 5)) should be(5)
+  }
+
+  it should "give an accurate count on right edge" in {
+    val grid =
+      (Location(6, 3) to Location(8, 7))
+        .foldLeft {
+          Grid.make(9, 9, Dead)
+        } { (grid, xy) =>
+          grid.set(xy, Alive)
+        }
+        .set(Location(8, 3), Dead)
+
+    grid.livingNeighborsAt(Location(8, 3)) should be(3)
+  }
+
+  it should "give an accurate count on top edge" in {
+    val grid = Grid
+      .make(9, 9, Alive)
+      .set(Location(5, 0), Dead)
+
+    grid.livingNeighborsAt(Location(5, 0)) should be(5)
+  }
+
+  it should "give an accurate count on bottom edge" in {
+    val grid = Grid
+      .make(9, 9, Alive)
+      .set(Location(5, 8), Dead)
+
+    grid.livingNeighborsAt(Location(5, 8)) should be(5)
+  }
+
+  it should "give an accurate count on top left corner" in {
+    val grid = Grid.make(9, 9, Alive)
+
+    grid.livingNeighborsAt(Location(0, 0)) should be(3)
+  }
+
+  it should "give an accurate count on top right corner" in {
+    val grid = Grid.make(9, 9, Alive)
+
+    grid.livingNeighborsAt(Location(8, 0)) should be(3)
+  }
+
+  it should "give an accurate count on bottom left corner" in {
+    val grid = Grid.make(9, 9, Alive)
+
+    grid.livingNeighborsAt(Location(0, 8)) should be(3)
+  }
+
+  it should "give an accurate count on bottom right corner" in {
+    val grid = Grid.make(9, 9, Alive)
+
+    grid.livingNeighborsAt(Location(8, 8)) should be(3)
   }
 }
